ae535c5095fed9c0d8427bb2cfc5763008c2b008
Jonathan Davis <jonathan@obj.co>
2018-06-22 00:49:03 -0600

    Adds PrimaryNav component

---
+++ src/components/Header/components/PrimaryNav/PrimaryNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import './PrimaryNav.css'
+
+export const PrimaryNav = props => (
+  <nav {...props} className={classNames('PrimaryNav', props.className)}>
+    <ul>
+      <li><NavLink to="/stack">Stack</NavLink></li>
+    </ul>
+  </nav>
+)
+
+PrimaryNav.propTypes = {
+  className: PropTypes.string
+}
+
+export default PrimaryNav
---
+++ src/components/Header/components/PrimaryNav/PrimaryNav.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { NavLink } from 'react-router-dom'
+
+import { PrimaryNav } from './PrimaryNav'
+
+describe('PrimaryNav component', () => {
+  let component
+
+  const props = {
+    id: 'mock-id',
+    'aria-labelledby': 'mock-label'
+  }
+
+  beforeAll(() => {
+    component = shallow(<PrimaryNav {...props} />)
+  })
+
+  it('should render', () => {
+    expect(component).toExist()
+  })
+
+  it('should set the className', () => {
+    expect(component.prop('className')).toBe('PrimaryNav')
+  })
+
+  it('should a link to the stack page', () => {
+    const link = component.find(NavLink).filterWhere(n => n.prop('to') === '/stack')
+    expect(link).toExist()
+  })
+
+  it('should spred the props', () => {
+    expect(component.props()).toMatchObject(props)
+  })
+
+  describe('when a className prop is set', () => {
+    beforeAll(() => {
+      component.setProps({
+        className: 'mockClassName'
+      })
+    })
+
+    it('should extend the className', () => {
+      expect(component.prop('className')).toBe('PrimaryNav mockClassName')
+    })
+  })
+})
