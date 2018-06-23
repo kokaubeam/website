a0de9148179ba86069dc2af3b59908448758b97b
Jonathan Davis <jonathan@obj.co>
2018-06-23 16:38:28 -0600

    Fixes Safari Invalid Date

--- src/views/WorkHistory/data.js
+++ src/views/WorkHistory/data.js
@@ -14,69 +14,69 @@ export default [{
   position: 'Senior Software Engineer',
   description: 'Developed content curation apps to integrate with data inference and aggregation services. Created internationalization tools to support initiatives into new world markets.',
   logo: ancestryLogo,
-  start: new Date('Mar 2012'),
-  end: new Date('Apr 2014')
+  start: new Date('Mar 1, 2012'),
+  end: new Date('Apr 30, 2014')
 }, {
   name: 'FamilyLink.com',
   position: 'Senior Software Engineer',
   description: 'Connected families through social media integration and full-stack development of web apps supporting over 56 million registered users. Assisted in the acquisition of wererelated.com by ancestry.com.',
   logo: familyLinkLogo,
-  start: new Date('Nov 2011'),
-  end: new Date('Mar 2012')
+  start: new Date('Nov 1, 2011'),
+  end: new Date('Mar 1, 2012')
 }, {
   name: 'Perfect Search',
   position: 'Senior Software Engineer',
   description: 'Developed web interfaces and supporting architecture. Prototyped reporting tools and visualized data.',
   logo: perfectSearchLogo,
-  start: new Date('May 2014'),
-  end: new Date('May 2015')
+  start: new Date('May 1, 2014'),
+  end: new Date('May 1, 2015')
 }, {
   name: 'Xennsoft',
   position: 'Quality Assurance Engineer',
   description: 'User experience and accessibility testing of web applications for multi-level marketing companies.',
   logo: xennsoftLogo,
-  start: new Date('Dec 2006'),
-  end: new Date('Mar 2008')
+  start: new Date('Dec 1, 2006'),
+  end: new Date('Mar 1, 2008')
 }, {
   name: 'Berkadia',
   position: 'Team Lead, Senior Software Engineer',
   description: 'Developing the rediq.io experience to provide valuations and comparisons of multi-family property investments.',
   logo: berkadiaLogo,
-  start: new Date('Oct 2017'),
+  start: new Date('Oct 1, 2017'),
   end: new Date()
 }, {
   name: 'The Objective Company',
   position: 'Owner',
   description: 'Start-up company founded to organize freelance projects into a formal business entity. Communicated directly with clients and independent contractors to extend the reach of their talents and services through technology.',
   logo: objectiveCompanyLogo,
-  start: new Date('Jun 2009'),
-  end: new Date('Aug 2016')
+  start: new Date('Jun 1, 2009'),
+  end: new Date('Aug 1, 2016')
 }, {
   name: 'Pluralsight',
   position: 'Senior Software Engineer',
   description: 'Developed the community and learning-path experiences for pluralsight.com using principles of test-driven development, pair-programming, and domain-driven design.',
   logo: pluralsightLogo,
-  start: new Date('May 2015'),
-  end: new Date('Sep 2017')
+  start: new Date('May 1, 2015'),
+  end: new Date('Sep 1, 2017')
 }, {
   name: 'Datamark',
   position: 'Quality Assurance Engineer',
   description: 'Testing of lead generation platforms. Created testing procedures and implemented automated testing.',
   logo: datamarkLogo,
-  start: new Date('Sep 2003'),
-  end: new Date('Oct 2004')
+  start: new Date('Sep 1, 2003'),
+  end: new Date('Oct 1, 2004')
 }, {
   name: 'Objective Play',
   position: 'Founder',
   description: 'Persuing a personal passion to create interactive digital media, such as games and virtual reality experiences. Learn more at objplay.com.',
   logo: objectivePlayLogo,
-  start: new Date('Mar 2017'),
+  start: new Date('Mar 1, 2017'),
   end: new Date()
 }, {
   name: 'Sebo Marketing',
   position: 'Software Engineer',
   description: 'Developed and maintained over 40 websites. Managed domains and servers supporting over 100 websites.',
   logo: seboMarketingLogo,
-  start: new Date('Mar 2008'),
-  end: new Date('Jun 2009')
+  start: new Date('Mar 1, 2008'),
+  end: new Date('Jun 1, 2009')
 }]
