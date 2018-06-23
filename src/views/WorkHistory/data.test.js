66bbec86eb67815c2fa7eb53266bcead04f26cbd
Jonathan Davis <jonathan@obj.co>
2018-06-23 02:40:28 -0600

    Adds WorkHistory data

---
+++ src/views/WorkHistory/data.test.js
@@ -0,0 +1,24 @@
+import data from './data'
+
+describe('WorkHistory data', () => {
+  it('should export an array', () => {
+    expect(data).toBeInstanceOf(Array)
+  })
+
+  it('should contain data', () => {
+    expect(data.length).toBeGreaterThan(0)
+  })
+
+  it('should contain all required attributes for each work history item', () => {
+    data.forEach(history => {
+      expect(history).toMatchObject({
+        name: expect.any(String),
+        position: expect.any(String),
+        description: expect.any(String),
+        logo: expect.any(String),
+        start: expect.any(Date),
+        end: expect.any(Date)
+      })
+    })
+  })
+})
