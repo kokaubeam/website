012f49c4b5cd8480dd94eaf96a56010a4bab152e
Jonathan Davis <jonathan@obj.co>
2018-06-25 19:10:18 -0600

    Adds WorkHistory constants

---
+++ src/components/WorkHistory/constants.test.js
@@ -0,0 +1,19 @@
+import {
+  WORK_HISTORY_REQUEST,
+  WORK_HISTORY_REQUEST_SUCCESS,
+  WORK_HISTORY_REQUEST_FAILURE
+} from './constants'
+
+describe('Stack constants', () => {
+  it('should export WORK_HISTORY_REQUEST', () => {
+    expect(WORK_HISTORY_REQUEST).toBeDefined()
+  })
+
+  it('should export WORK_HISTORY_REQUEST_SUCCESS', () => {
+    expect(WORK_HISTORY_REQUEST_SUCCESS).toBeDefined()
+  })
+
+  it('should export WORK_HISTORY_REQUEST_FAILURE', () => {
+    expect(WORK_HISTORY_REQUEST_FAILURE).toBeDefined()
+  })
+})
